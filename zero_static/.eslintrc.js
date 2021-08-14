5eb3eeaf4f412e8574dd9e2a0ad4d5a97283c164
moyang <moyanghualing@163.com>
2021-08-15 02:20:23 +0800

    格式化+环境搭建

--- zero_static/.eslintrc.js
+++ zero_static/.eslintrc.js
@@ -9,5 +9,8 @@ module.exports = {
         parser: 'babel-eslint'
     },
 
-    rules: {}
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    }
 }
